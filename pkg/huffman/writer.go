d1fb91ca39fcc41684829e791d1be290b48ae0ed
rob <robgrovesemail@gmail.com>
2017-08-26 08:18:00 -0700

    Add constructor for huffman.Writer
    At the momement, the goal is to make huffman.Writer implement the Writer
    interface. To this end, huffman.Writer contains encTable, currByte, and
    bitsWritten fields. The idea is to have the writer look at a byte,
    encode it, then write the result to its underlying writer field ('w').

--- pkg/huffman/writer.go
+++ pkg/huffman/writer.go
@@ -1,1 +1,39 @@
 package huffman
+
+//#############################################################################
+//# Exported
+//#############################################################################
+
+type Writer struct {
+	Header      huffmanHeader
+	w           io.Writter
+	encTable    *encodingTable
+	codebook    *Codebook
+	currByte    byte
+	bitsWritten int
+}
+
+func NewWriter(w io.Writer, cb *Codebook) *Writter {
+	encTable := make(map[byte]string)
+	for _, entry := range cb {
+		encTable[entry.unit] = entry.code
+	}
+	return &Writer{
+		Header:      "",
+		w:           w,
+		encTable:    encTable,
+		codebook:    cb,
+		currByte:    0,
+		bitsWritten: 0,
+	}
+}
+
+//#############################################################################
+//# Unexported
+//#############################################################################
+type huffmanHeader struct {
+	bitLens  []byte
+	alphabet []byte
+}
+
+type encodingTable map[byte]string
