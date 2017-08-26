fc4f6c38ebaede1a40bbbad7206ef7226d55bc99
rob <robgrovesemail@gmail.com>
2017-08-26 03:15:04 -0700

    Add function to create Huffman Trees
    With newHuffmanTree(), we now have a means to generate Huffman Trees,
    and can now derive a codebook.
    NOTE: I've decided to have the function return the leaves of the tree
    rather than its root. I've done this for two reasons:
    1. Callers of the function (i.e those which derive a codebook) need to
    know the values stored in the leaves.
    2. Its easier to find the root from a leaf than vice-versa.

--- pkg/huffman/codebook.go
+++ pkg/huffman/codebook.go
@@ -1,5 +1,16 @@
 package huffman
 
+import (
+	"container/heap"
+)
+
+//#############################################################################
+//# Exported
+//#############################################################################
+
+//#############################################################################
+//# Unexported
+//#############################################################################
 //-----------------------------------------------------------------------------
 //- Huffman Tree
 //-----------------------------------------------------------------------------
@@ -15,7 +26,7 @@ type huffTreePQ []*huffTree
 
 func (htpq huffTreePQ) Len() int { return len(htpq) }
 
-func (htpq huffTreePQ) Less(i, j int) {
+func (htpq huffTreePQ) Less(i, j int) bool {
 	return htpq[i].weight < htpq[j].weight
 }
 
@@ -34,4 +45,31 @@ func (htpq *huffTreePQ) Pop() interface{} {
 	item := old[n-1]
 	*htpq = old[0 : n-1]
 	return item
+}
+
+// newHuffTree creates a new Huffman tree.
+// The function returns the leaves of the tree, rather than its root.
+// This was done because most callers of this function want access to the
+// tree's leaves.
+func newHuffTree(weightMap map[byte]float64) (leaves []*huffTree) {
+	treePQ := make(huffTreePQ, len(weightMap))
+	i := 0
+	for b, w := range weightMap {
+		leaf := &huffTree{b, w, nil, nil, nil}
+		treePQ[i] = leaf
+		leaves[i] = leaf
+		i++
+	}
+	heap.Init(&treePQ)
+
+	for treePQ.Len() > 1 {
+		min1 := heap.Pop(&treePQ).(*huffTree)
+		min2 := heap.Pop(&treePQ).(*huffTree)
+		comb := &huffTree{0, min1.weight + min2.weight, min1, min2, nil}
+		min1.parent = comb
+		min2.parent = comb
+		heap.Push(&treePQ, comb)
+	}
+
+	return
 }
