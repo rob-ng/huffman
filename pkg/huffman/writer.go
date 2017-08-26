dd458c4759f3327b0ac2bb4ff4939c283ada21a2
rob <robgrovesemail@gmail.com>
2017-08-26 10:34:26 -0700

    Add Write and Flush methods to huffman.Writer
    Together, Write() and Flush() allow a huffman.Writer to encode a data
    stream using Huffman coding. Note that (at least as implemented now) any
    writes to a huffman.Writer via Write() must be followed up with Flush().
    This ensures that any remaining bits in currByte (effectively a buffer
    of bits) are also written.

--- pkg/huffman/writer.go
+++ pkg/huffman/writer.go
@@ -1,31 +1,79 @@
 package huffman
 
+import (
+	"fmt"
+	"io"
+)
+
 //#############################################################################
 //# Exported
 //#############################################################################
 
+// A Writer
 type Writer struct {
 	Header      huffmanHeader
-	w           io.Writter
-	encTable    *encodingTable
-	codebook    *Codebook
+	wroteHeader bool
+	w           io.Writer
+	encTable    encodingTable
+	codebook    Codebook
 	currByte    byte
 	bitsWritten int
 }
 
-func NewWriter(w io.Writer, cb *Codebook) *Writter {
-	encTable := make(map[byte]string)
+// NewWriter acts as a constructor for Writer.
+func NewWriter(w io.Writer, cb Codebook) *Writer {
+	encTable := make(encodingTable)
 	for _, entry := range cb {
 		encTable[entry.unit] = entry.code
+		fmt.Printf("unit: %b, code: %s\n", entry.unit, entry.code)
 	}
 	return &Writer{
-		Header:      "",
+		Header:      huffmanHeader{},
+		wroteHeader: false,
 		w:           w,
 		encTable:    encTable,
 		codebook:    cb,
 		currByte:    0,
 		bitsWritten: 0,
 	}
+}
+
+// Write writes an encoded form of p to the underlying io.Writer.
+// Note that final value of currByte is not guaranteed to be written and hence
+// calls to Write() should be followed with a call to Flush().
+func (hw *Writer) Write(p []byte) (n int, err error) {
+	n = 0
+	if !hw.wroteHeader {
+		hw.wroteHeader = true
+		fmt.Printf("writing header...")
+	}
+	for _, b := range p {
+		bits := hw.encTable[b]
+		for _, bit := range bits {
+			hw.currByte = (hw.currByte << 1) | byte(bit-'0')
+			hw.bitsWritten++
+			if hw.bitsWritten == 8 {
+				hw.w.Write([]byte{hw.currByte})
+				hw.currByte = 0
+				hw.bitsWritten = 0
+				n++
+			}
+		}
+	}
+	return
+}
+
+// Flush fills the rest of currByte with 0 bits before writing it to the
+// underlying io.Writter.
+func (hw *Writer) Flush() error {
+	if hw.bitsWritten != 0 {
+		for hw.bitsWritten < 8 {
+			hw.currByte = (hw.currByte << 1) | 0
+			hw.bitsWritten++
+		}
+		hw.w.Write([]byte{hw.currByte})
+	}
+	return nil
 }
 
 //#############################################################################
