94dab929fa1217193c481c01f24a0a680afd8839
rob <robgrovesemail@gmail.com>
2017-08-28 14:42:03 -0700

    Implement Read method for huffman.Reader
    A huffman.Reader uses its Read() method to do the following:
    1. Read a byte via its underlying bufio.Reader
    2. Read through the byte as a series of bits.
       Bits are stored in a buffer (readBuf). When the value in the
       buffer matches a key in huffman.Reader's decoding table (and the
       number of bits read matches the corresponding code length), the
       decoded value given by the table is inserted into the byte array
       being read into.
    3. After a successful match, the buffer and number of bits read are
       reset and process continues.
    
    It should be noted that the method is not quite complete. Most
    glaringly, it doesn't have a way to terminate correctly after all the
    bytes have been read. This will likely require either including the
    number of encoded values in the header (to compare against as values are
    read into the byte array), or will require creating some king of EOF bit
    pattern to check against.
    Given the potential size of this change, it will have to wait to be
    part of a project-wide refactoring which I'll be starting soon.

--- pkg/huffman/reader.go
+++ pkg/huffman/reader.go
@@ -2,7 +2,7 @@ package huffman
 
 import (
 	"bufio"
-	"fmt"
+	"io"
 	"strconv"
 	"strings"
 )
@@ -10,8 +10,9 @@ import (
 type Reader struct {
 	headerRead bool
 	r          *bufio.Reader
-	readBuf    byte
-	cb         Codebook
+	readBuf    int
+	bitsRead   int
+	decTable   decodeTable
 }
 
 type Header struct {
@@ -20,11 +21,13 @@ type Header struct {
 }
 
 func NewReader(r *bufio.Reader) *Reader {
+	dt := make(decodeTable)
 	return &Reader{
 		headerRead: false,
 		r:          r,
 		readBuf:    0,
-		cb:         nil,
+		bitsRead:   0,
+		decTable:   dt,
 	}
 }
 
@@ -32,17 +35,56 @@ func (hr *Reader) Read(p []byte) (n int, err error) {
 	if !hr.headerRead {
 		hr.headerRead = true
 		header := hr.readHeader()
-		fmt.Printf("%v\n", header)
+		cb := DeriveCanonicalCB(header)
+		for _, entry := range cb {
+			hr.decTable[entry.code] = &decodeTableEntry{
+				unit:    entry.unit,
+				codeLen: entry.codeLen,
+			}
+		}
 	}
 
-	return 0, nil
+	n = 0
+	bi := 0
+	for i := 0; i < len(p); i++ {
+		currByte, _ := hr.r.ReadByte()
+		var j uint
+		for ; j < 8; j++ {
+			currBit := int((currByte >> (7 - j)) & 1)
+			hr.readBuf = (hr.readBuf << 1) | currBit
+			hr.bitsRead++
+			if n >= len(p) {
+				return 0, io.EOF
+			}
+			if _, ok := hr.decTable[hr.readBuf]; ok {
+				if hr.bitsRead == hr.decTable[hr.readBuf].codeLen {
+					p[bi] = hr.decTable[hr.readBuf].unit
+					bi++
+					n++
+					hr.bitsRead = 0
+					hr.readBuf = 0
+				}
+			}
+		}
+	}
+
+	return n, nil
+}
+
+type decodeTable map[int]*decodeTableEntry
+
+type decodeTableEntry struct {
+	unit    byte
+	codeLen int
 }
 
 func (hr *Reader) readHeader() *Header {
 	h1, _ := hr.r.ReadString('\n')
+	//h1, _ := hr.r.ReadBytes('\n')
 	h2, _ := hr.r.ReadBytes('\n')
 
 	h1Split := strings.Split(strings.Trim(h1, "\n"), " ")
+	h2 = h2[0 : len(h2)-1]
 
 	lenCounts := make([]int, len(h1Split))
 	for i, c := range h1Split {
@@ -53,4 +95,5 @@ func (hr *Reader) readHeader() *Header {
 		lenCounts,
 		h2,
 	}
+	return nil
 }
