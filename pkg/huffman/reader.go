d3f049c77b6c176e525a450897b083b05a38aa2d
rob <robgrovesemail@gmail.com>
2017-08-27 19:37:15 -0700

    Add Reader type and function to read header
    The huffman.Reader type is intended to decode encoded data as it is
    read. There are three stages to this process:
    1. Read the header from the encoded file.
    2. Derive a codebook / decoding table from the header.
    3. Use the decoding table to decode the body of the file.
    
    This commit handles the first of these stages.

--- pkg/huffman/reader.go
+++ pkg/huffman/reader.go
@@ -1,1 +1,56 @@
 package huffman
+
+import (
+	"bufio"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Reader struct {
+	headerRead bool
+	r          *bufio.Reader
+	readBuf    byte
+	cb         Codebook
+}
+
+type Header struct {
+	lenCounts []int
+	alphabet  []byte
+}
+
+func NewReader(r *bufio.Reader) *Reader {
+	return &Reader{
+		headerRead: false,
+		r:          r,
+		readBuf:    0,
+		cb:         nil,
+	}
+}
+
+func (hr *Reader) Read(p []byte) (n int, err error) {
+	if !hr.headerRead {
+		hr.headerRead = true
+		header := hr.readHeader()
+		fmt.Printf("%v\n", header)
+	}
+
+	return 0, nil
+}
+
+func (hr *Reader) readHeader() *Header {
+	h1, _ := hr.r.ReadString('\n')
+	h2, _ := hr.r.ReadBytes('\n')
+
+	h1Split := strings.Split(strings.Trim(h1, "\n"), " ")
+
+	lenCounts := make([]int, len(h1Split))
+	for i, c := range h1Split {
+		lenCounts[i], _ = strconv.Atoi(c)
+	}
+
+	return &Header{
+		lenCounts,
+		h2,
+	}
+}
