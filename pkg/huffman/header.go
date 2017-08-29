203231f070200d76009cd95bd9ef1552fc8d2048
rob <robgrovesemail@gmail.com>
2017-08-28 22:53:36 -0700

    Add String method to Header
    The String() method returns a formatted string describing the Header.
    This string will be written to a file during encoding, and be used
    during decoding to decode the body of the file.
    
    For the moment, the string consists of four parts:
    1. A space separated list of integers describing the number of codes
    whose code length == (index in list + 1).
    2. A set of all units in the file.
    3. The number of units in the file.
    4. A delimiter indicating the end of the header (headerDelim).
    
    The idea behind the final delimiter is to allow huffman.Reader's Read()
    function to easily extract the Header description. As the structure of
    the string generated by String() might change in the future, this
    helps to decouple Read() from the method's implementation.

--- pkg/huffman/header.go
+++ pkg/huffman/header.go
@@ -1,26 +1,58 @@
 package huffman
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 //=============================================================================
 //= Exported
 //=============================================================================
 
 type Header struct {
-	cb       Codebook
+	cb       codebook
 	numUnits int
+}
+
+func (h *Header) String() string {
+	maxBitLen := 0
+	for _, e := range h.cb {
+		if e.codeLen > maxBitLen {
+			maxBitLen = e.codeLen
+		}
+	}
+	totals := make([]int, maxBitLen)
+	units := make([]byte, len(h.cb))
+	for i, e := range h.cb {
+		totals[e.codeLen-1]++
+		units[i] = e.unit
+	}
+	totalsStr := make([]string, maxBitLen)
+	for i, _ := range totals {
+		totalsStr[i] = strconv.Itoa(totals[i])
+	}
+	numUnits := h.numUnits
+
+	line1 := strings.Join(totalsStr, " ")
+	line2 := units
+	line3 := numUnits
+
+	return fmt.Sprintf("%s\n%s\n%d\n%s", line1, line2, line3, headerDelim)
 }
 
 // NewHeader creates and returns a pointer to a new Header.
 // To do this, it needs to be given the number of units (bytes) in the file, as
 // well as a weight for each such unit (usually its frequency within the file).
 func NewHeader(unitWeights map[byte]float64, numUnits int) *Header {
-
+	return nil
 }
 
 // DeriveHeader recreates the Header described by headerDesc.
 // In nearly all cases, headerDesc should be the first n lines of an encoded
 // file, the last ending with 'headerDelim'.
 func DeriveHeader(headerDesc string) *Header {
-
+	return nil
 }
 
 //=============================================================================
@@ -34,4 +66,4 @@ type cbEntry struct {
 	code    int
 	codeLen int
 }
-type Codebook []*cbEntry
+type codebook []*cbEntry
