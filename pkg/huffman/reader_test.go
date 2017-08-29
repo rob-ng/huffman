ce53da8385cb2b256f8921b1b92178e15b60c947
rob <robgrovesemail@gmail.com>
2017-08-29 14:15:19 -0700

    Update existing tests to use new Header type

--- pkg/huffman/reader_test.go
+++ pkg/huffman/reader_test.go
@@ -3,13 +3,14 @@ package huffman
 import (
 	"bufio"
 	"bytes"
+	"strings"
 	"testing"
 )
 
 func TestRead(t *testing.T) {
-	//utf8src := "©©©»»»»かかπ"
-	//br := bufio.NewReader(strings.NewReader(utf8src))
-	//utf8WM, _ := MakeWeightMap(br)
+	utf8src := "©©©»»»»かかπ"
+	br := bufio.NewReader(strings.NewReader(utf8src))
+	utf8WM, n, _ := ProcessData(br) //MakeWeightMap(br)
 	testInputs := []struct {
 		src string
 		//cb  Codebook
@@ -23,10 +24,10 @@ func TestRead(t *testing.T) {
 				51: 0.2,
 				52: 0.1,
 			}, 10),
-		}, /*{
+		}, {
 			utf8src,
-			NewHeader(utf8WM, 10),
-		},*/
+			NewHeader(utf8WM, n),
+		},
 	}
 
 	for _, ti := range testInputs {
@@ -39,14 +40,22 @@ func TestRead(t *testing.T) {
 		r := bufio.NewReader(&out)
 		hr := NewReader(r)
 
-		out2 := make([]byte, 10)
+		out2 := make([]byte, ti.h.numUnits+10)
 		hr.Read(out2)
 
-		for i, _ := range out2 {
-			if out2[i] != ti.src[i] {
-				t.Errorf("Decoded data should equal source data. Was: %d, Expected; %d\n",
+		inBytes := []byte(ti.src)
+		for i, _ := range inBytes {
+			if out2[i] != inBytes[i] {
+				t.Errorf("Decoded data should equal source data. Was: %d, Expected: %d\n",
 					out2[i], ti.src[i])
 			}
 		}
+		for i := ti.h.numUnits; i < ti.h.numUnits+10; i++ {
+			if out2[i] != 0 {
+				t.Errorf("Should only have read %d bytes to out", ti.h.numUnits)
+			}
+		}
+
+		//fmt.Printf("%s\n", out2)
 	}
 }
