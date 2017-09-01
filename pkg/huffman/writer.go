fddad5df78daf7dbc738510ab4d660bbe948e76b
rob <robgrovesemail@gmail.com>
2017-08-31 20:05:27 -0700

    Improve Write() by using internal buffer
    Currently, Write() writes bytes individually as they are created. This
    is very slow. To improve Write()'s performance, I've added an internal
    buffer to store encoded bytes as they are written. Once the buffer is
    filled, the entire buffer is written to Writer's underlying io.Writer.
    This cuts the number of writes significantly and thus boosts
    performance.
    
    It should be noted that this buffer exists on a per-call basis.
    Everything stored within the buffer is guaranteed to be written before
    the function completes.

--- pkg/huffman/writer.go
+++ pkg/huffman/writer.go
@@ -8,6 +8,8 @@ import (
 //# Exported
 //#############################################################################
 
+const writeBufSize = 1024
+
 // A Writer writes encoded data to an underlying io.Writer.
 type Writer struct {
 	wroteHeader bool
@@ -34,6 +36,7 @@ func NewWriter(w io.Writer, h *Header) *Writer {
 }
 
 // Write writes an encoded form of p to the underlying io.Writer.
+// Write uses an internal buffer to store encoded bytes as they are created.
 // Note that final value of currByte is not guaranteed to be written and hence
 // calls to Write() should be followed with a call to Flush().
 func (hw *Writer) Write(p []byte) (int, error) {
@@ -44,16 +47,17 @@ func (hw *Writer) Write(p []byte) (int, error) {
 	var n int
 	if !hw.wroteHeader {
 		hw.wroteHeader = true
-		_, hw.err = hw.w.Write([]byte(hw.h.String()))
-		if hw.err != nil {
+		if _, hw.err = hw.w.Write([]byte(hw.h.String())); hw.err != nil {
 			return 0, hw.err
 		}
 	}
+	wBuf := make([]byte, writeBufSize)
+	wi := 0
 	for _, b := range p {
 		enc := hw.encoder[b]
 		bitArray := make([]int, enc.codeLen)
 		for i := 0; i < enc.codeLen; i++ {
-			// Repeatedly get least significant bit
+			// Repeatedly get least significant bit from code
 			bitArray[enc.codeLen-1-i] = enc.code & -enc.code
 			enc.code >>= 1
 		}
@@ -61,18 +65,25 @@ func (hw *Writer) Write(p []byte) (int, error) {
 			hw.currByte = (hw.currByte << 1) | byte(bit)
 			hw.bitsWritten++
 			if hw.bitsWritten == 8 {
+				wBuf[wi] = hw.currByte
+				wi++
+				hw.currByte = 0
+				hw.bitsWritten = 0
+			}
+			if wi == len(wBuf) {
 				var newWritten int
-				newWritten, hw.err = hw.w.Write([]byte{hw.currByte})
+				newWritten, hw.err = hw.w.Write(wBuf[:wi])
 				if hw.err != nil {
 					return n + newWritten, hw.err
 				}
-				hw.currByte = 0
-				hw.bitsWritten = 0
 				n += newWritten
+				wi = 0
 			}
 		}
 	}
-	return n, hw.err
+	var newWritten int
+	newWritten, hw.err = hw.w.Write(wBuf[:wi])
+	return n + newWritten, hw.err
 }
 
 // Flush fills the rest of currByte with 0 bits before writing it to the
